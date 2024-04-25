2bcd840f2067ce967a8624781de4e0e8caca7497
ibtisamath <ibtisamathsoumanou1@gmail.com>
2024-04-25 12:10:31 +0200

    home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from "./layouts/Footer/Footer";
 import Header from "./layouts/Header/Header";
 import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
@@ -16,9 +15,7 @@ const store = configureStore({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store} >
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </Provider>
 );
 

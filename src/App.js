2d4e4e93b316e9efe6ca8f5fafed05fb15e38b1d
ibtisamath <ibtisamathsoumanou1@gmail.com>
2024-04-25 14:08:59 +0200

    home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from "./layouts/Footer/Footer";
 import Header from "./layouts/Header/Header";
 import Home from "./pages/Home/Home";
+import "./App.css";
+
 
 const App = ()=> {
   return (
